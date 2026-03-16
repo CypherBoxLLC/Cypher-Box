ad5def808a731d1bb6f97232a519d65b192cbc59
Basem Alasi <kaliko@Basems-MacBook-Pro.local>
2026-03-16 18:24:12 +0100

    fix(capsule): match ListView height to image height (124)

--- src/screens/HotStorageVault/ListView/styles.ts
+++ src/screens/HotStorageVault/ListView/styles.ts
@@ -24,7 +24,7 @@ export default StyleSheet.create<Style>({
     },
     main: {
         width: widths,
-        height: 100,
+        height: 124,
         marginStart: 5,
         marginVertical: 5
     },
