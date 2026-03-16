30186c22fdfda28ac677ae3f7c1296ce18ce28a6
Basem Alasi <kaliko@Basems-MacBook-Pro.local>
2026-03-16 19:22:25 +0100

    fix: raise selection border

--- src/screens/HotStorageVault/ListView/styles.ts
+++ src/screens/HotStorageVault/ListView/styles.ts
@@ -90,7 +90,7 @@ export default StyleSheet.create<Style>({
         borderRadius: 25,
         width: '95%',
         height: '95%',
-        top: 10,
+        top: 0,
         start: 5,
         position: 'absolute',
     },
